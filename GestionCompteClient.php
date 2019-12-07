5eecf1c0cec828da26f7223aa96cdd628ea2d623
inabernic <bernic.ina@gmail.com>
2019-12-07 17:36:44 +0100

    compte-client

---
+++ GestionCompteClient.php
@@ -0,0 +1,73 @@
+ <?php
+	header("charset:utf8");
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: access");
+	header("Access-Control-Allow-Methods: *");
+	header("Content-Type: application/json; charset=UTF-8");
+	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+	
+	// Connect to database
+	include("db_connect.php");
+	$request_method = $_SERVER["REQUEST_METHOD"];
+
+	function getGestionCompteClients()
+	{   
+		global $conn;
+		$query = //"SELECT * FROM compte";
+		" select nom, prenom, num_rue, nom_rue, code_postal, ville, id_compte, rib, solde, type_compte from utilisateur inner join adresse on adresse.id_user=utilisateur.id_user inner join client on utilisateur.id_user=client.id_user inner join compte on client.id_client=compte.id_client ";
+		$response = array();
+		$result = mysqli_query($conn, $query);
+		
+		while($row = mysqli_fetch_array($result))
+		{
+			$response[] = $row;
+			
+		}
+		header('Content-Type: application/json');
+	    echo json_encode($response, JSON_PRETTY_PRINT);
+		
+		
+	}
+	
+	function getGestionCompteClient($id_client)
+	{
+		global $conn;
+		$query = //"SELECT * FROM compte";
+		$query = " select nom, prenom, num_rue, nom_rue, code_postal, ville, id_compte, rib, solde, type_compte from utilisateur inner join adresse on adresse.id_user=utilisateur.id_user inner join client on utilisateur.id_user=client.id_user inner join compte on client.id_client=compte.id_client ";
+		if($id_client != 0)
+		{
+			$query .= " WHERE client.id_client=".$id_client." LIMIT 1";
+		}
+		$response = array();
+		$result = mysqli_query($conn, $query);
+		while($row = mysqli_fetch_array($result))
+		{
+			$response[] = $row;
+		}
+		header('Content-Type: application/json');
+		echo json_encode($response, JSON_PRETTY_PRINT);
+	}
+		
+	// Orchestration des différentes fonctions
+	switch($request_method)
+	{
+		
+		case 'GET':
+			// Retrive Products
+			if(!empty($_GET["id_client"]))
+			{
+				$id_client=intval($_GET["id_client"]);
+				getGestionCompteClient($id_client);
+			}
+			else
+			{
+				getGestionCompteClients();
+			}
+			break;
+		default:
+			// Invalid Request Method
+			header("HTTP/1.0 405 Method Not Allowed");
+			break;
+			
+	}
+?>
--- gk-force/src/app/compte-client/compte-client.component.ts
+++ gk-force/src/app/compte-client/compte-client.component.ts
@@ -1,30 +1,94 @@
 import { Component, OnInit } from '@angular/core';
-import { Compte } from '../modeles/Compte';
-import { CompteService } from '../services/CompteService';
+import { GestionCompteClient } from '../modeles/GestionCompteClient';
+import { GestionCompteClientService } from '../services/GestionCompteClientService';
+
 
 @Component({
   selector: 'app-compte-client',
   templateUrl: './compte-client.component.html',
   styleUrls: ['./compte-client.component.scss'],
-  providers: [CompteService]
+  providers: [GestionCompteClientService]
 })
 export class CompteClientComponent implements OnInit {
 
-  private compte: Compte[];
+  private gestionCompteClient : GestionCompteClient[];
 
-  constructor(private serviceCompte:CompteService) { }
+  constructor(
+    private serviceGestionCompteClient:GestionCompteClientService,
+    ) { }
 
   ngOnInit() 
   {
-    this.getCompte();
+    this.getGestionCompteClient();
   }
 
-  getCompte()
+  getAdresseEntete()
   {
-    this.serviceCompte.findAll().subscribe
+      let adresse='';
+      if (this.gestionCompteClient.length!=0)
+      {
+          adresse=this.gestionCompteClient[0].nom
+                + " " + this.gestionCompteClient[0].prenom;
+      } 
+      return adresse;
+  }
+
+  getAdresseCorps()
+  {
+      let adresse='';
+      if (this.gestionCompteClient.length!=0)
+      {
+          adresse=this.gestionCompteClient[0].num_rue
+                + ", " + this.gestionCompteClient[0].nom_rue;
+      } 
+      return adresse;
+  }
+
+  getAdressePied()
+  {
+      let adresse='';
+      if (this.gestionCompteClient.length!=0)
+      {
+          adresse=this.gestionCompteClient[0].code_postal
+                + " " + this.gestionCompteClient[0].ville;
+      } 
+      return adresse;
+  }
+
+  getGestionCompteClient()
+  {
+    this.serviceGestionCompteClient.findAll().subscribe
     (
-      cpt=>{this.compte=cpt;}
+      gestcptcli=>{this.gestionCompteClient=gestcptcli;}
     )
+  }
+
+  getTypeCompteLibelle($CodeCompte)
+  {
+    let libelle='';
+      if (this.gestionCompteClient.length!=0)
+      {
+          if ($CodeCompte==0)
+          {
+            libelle="renuméré";
+          }
+          else
+          {
+            libelle="courant";
+          }
+      } 
+      return libelle;
+  }
+
+ getTotal()
+  {
+    let total = 0;
+    for (var i = 0; i < this.gestionCompteClient.length; i++) {
+        if (this.gestionCompteClient[i].solde) {
+           total = Number.parseFloat(total.toString()) + Number.parseFloat(this.gestionCompteClient[i].solde.toString());
+        }
+    }
+    return total;
   }
 
 }
